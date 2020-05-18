ff5e712c88fd76eb6a8ce7c368a30cd58b483353
Piyu <siddhesh.parab28@gmail.com>
2020-05-18 17:56:02 +0530

    new

---
+++ lib/splash.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flare_splash_screen/flare_splash_screen.dart';
+import 'package:listview/pages/team_member_pages.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Splash extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    Future data;
+    Future getsData()async{
+      var firestore =Firestore.instance;
+      QuerySnapshot qp=await firestore.collection("posts").getDocuments();
+
+    SplashScreen.navigate(
+      name: "assets/intro.flr",
+      next: Search(),
+      width: double.infinity,
+      height: double.infinity,
+      alignment: Alignment.center,
+      until: () => Future.delayed(Duration(seconds: 5)),
+      backgroundColor: Colors.white,
+      startAnimation: "coding",
+    );
+
+    return qp.documents;
+  }
+}}
