053eb7dd9759fd973289c54195610478cf40eee2
Nitish Gupta <nitish8040@gmail.com>
2020-10-01 00:04:20 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.green,
           canvasColor: Colors.white
       ),
       home: Splash(),
